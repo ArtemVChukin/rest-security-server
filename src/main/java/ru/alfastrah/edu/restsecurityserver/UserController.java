d196e32d2e3319fa6329ba22648de1038c3410db
ArtemVChukin <ArtemVChukin@gmail.com>
2020-12-20 20:39:50 +0300

    simplify jwt auth

--- src/main/java/ru/alfastrah/edu/restsecurityserver/UserController.java
+++ src/main/java/ru/alfastrah/edu/restsecurityserver/UserController.java
@@ -1,35 +1,22 @@
 package ru.alfastrah.edu.restsecurityserver;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(UserController.USER_BASE_URL)
 public class UserController {
-    public static final String USER_BASE_URL = "/user";
-    private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
-    @GetMapping
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-    @GetMapping("/{username}")
-    public User getUser(@PathVariable String username) {
-        return userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
-    }
-
-    @PostMapping
+    @PostMapping("/user")
     @SuppressWarnings("java:S4684")
-    public User saveUser(@RequestBody @Valid User user) {
+    public User addUser(@RequestBody @Valid User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
